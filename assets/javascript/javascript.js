fd00d8847c8f22582da3d2c4b339ebeda8dc9126
Tyler G <guthriet1230@gmail.com>
2018-10-02 12:27:41 -0500

    work on gameover

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,31 +2,66 @@
 // Variables
 /////////////////////////////////////////////////////////
 var questions = [
-"It's really cold outside, they are calling it a major freeze, weeks ahead of normal. Man, we could use a big fat dose of global warming!",
+  {
+    question:
+      "It's really cold outside, they are calling it a major freeze, weeks ahead of normal. Man, we could use a big fat dose of global warming!",
+    answer: true
+  },
 
+  {
+    question:
+      "We're rounding 'em up in a very humane way, in a very nice way. And they're going to be happy because they want to be legalized. And, by the way, I know it doesn't sound nice. But not everything is nice.",
+    answer: true
+  },
 
-  "We're rounding 'em up in a very humane way, in a very nice way. And they're going to be happy because they want to be legalized. And, by the way, I know it doesn't sound nice. But not everything is nice.",
+  {
+    question:
+      "I will go down as the voice of this generation, of this decade, I will be the loudest voice.",
+    answer: false
+  },
 
-  "I will build a great, great wall on our southern border, and I will have Mexico pay for that wall. Mark my words.",
+  {
+    question:
+      "I have a great relationship with African Americans, as you possibly have heard. I just have great respect for them. And they like me. I like them.",
+    answer: true
+  },
+  {
+    question:
+      "The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive.",
+    answer: true
+  },
 
-  "I have a great relationship with African Americans, as you possibly have heard. I just have great respect for them. And they like me. I like them.",
+  {
+    question:
+      "To be blunt, people would vote for me. They just would. Why? Maybe because I'm so good looking.",
+    answer: true
+  },
 
-  "The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive.",
+  {
+    question:
+      "You basically can say anything to someone on an email or text as long as you put an LOL at the end.",
+    answer: false
+  },
 
-  "To be blunt, people would vote for me. They just would. Why? Maybe because I'm so good looking.",
+  {
+    question:
+      "I’ll say things that are serious and put them in a joke form so people can enjoy them. We laugh to keep from crying.",
+    answer: false
+  },
 
-  "I will go down as the voice of this generation, of this decade, I will be the loudest voice.",
+  {
+    question:
+      "I will build a great, great wall on our southern border, and I will have Mexico pay for that wall. Mark my words.",
+    answer: true
+  },
 
-  "You basically can say anything to someone on an email or text as long as you put an LOL at the end.",
-
-  "I’ll say things that are serious and put them in a joke form so people can enjoy them. We laugh to keep from crying.",
-
-  "People say I have a bad reputation. I think I’ve got the best reputation in the building."
+  {
+    question:
+      "People say I have a bad reputation. I think I’ve got the best reputation in the building.",
+    answer: false
+  }
 ];
-questions[0, 1, 2, 3, 4, 5] = true;
-questions[6, 7, 8, 9] = false;
-var trueAnswer = true;
-var falseAnswer = false;
+
 var tenTimer = 11;
 var correct = 0;
 var wrong = 0;
@@ -41,9 +76,12 @@ $(document).ready(function() {
   $("#start-button").on("click", function() {
     console.log("I've been clicked!");
     $("#start-button").remove();
-    $('.instructions').remove();
-    $("#question-display").text(questions[count]);
+    $(".instructions").remove();
+    $("#question-display").text(questions[count].question);
     run();
+    if (correct + wrong === 10){
+      gameOver();
+    }
 
     /////////////////////////////////////////////////////////
     // Timer Functions
@@ -82,19 +120,21 @@ $(document).ready(function() {
       intervalId = setInterval(decrement, 1000);
       decrement();
       count++;
-      $("#question-display").text(questions[count]);
+      $("#question-display").text(questions[count].question);
     }
 
     /////////////////////////////////////////////////////////
     // True Click
     /////////////////////////////////////////////////////////
     $("#true").on("click", function() {
-      alert("truth");
-      if (question[count] === true) {
+      // alert("truth");
+      if (questions[count].answer === true) {
         correct++;
+        $("#Correct-answers").text(correct);
         nextQuestion();
       } else {
         wrong++;
+        $("#Wrong-answers").text(wrong);
         nextQuestion();
       }
     });
@@ -103,15 +143,27 @@ $(document).ready(function() {
     // False Click
     /////////////////////////////////////////////////////////
     $("#false").on("click", function() {
-      alert("false");
-      if (question[count] === false) {
+      // alert("false");
+      if (questions[count] === false) {
         correct++;
+        $("#Correct-answers").text(correct);
         nextQuestion();
       } else {
         wrong++;
+        $("#Wrong-answers").text(wrong);
         nextQuestion();
       }
     });
+ /////////////////////////////////////////////////////////
+    // Gameover
+    /////////////////////////////////////////////////////////
+    function gameOver() {
+      alert("Game is Over");
+    }
+
+
+
+
   });
 });
 
